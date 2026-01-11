2f0d00d292b1cc2d4e34a4848fe1c0004759e775
Krzysztof Bużko <k.buzko@gmail.com>
2026-01-11 11:57:31 +0100

    feat: Producer:getAll update according to docs

--- src/Repository/ProducerRepository.php
+++ src/Repository/ProducerRepository.php
@@ -16,8 +16,19 @@ class ProducerRepository extends AbstractRepository
     public function getAll(): array
     {
         $data = $this->request('GET', self::PATH);
-        // possible shapes: ['items'=>[...]] or ['producers'=>[...]] or direct array
-        $items = $data['items'] ?? $data['producers'] ?? $data;
+
+        // możliwe kształty: ['producers' => [...]] lub lista [...], lub pojedynczy obiekt
+        if (isset($data['producers']) && is_array($data['producers'])) {
+            $items = $data['producers'];
+        } elseif (is_array($data) && self::isList($data)) {
+            $items = $data;
+        } elseif (is_array($data) && isset($data['id'])) {
+            // pojedynczy obiekt zwrócony bez otaczającej listy
+            $items = [$data];
+        } else {
+            $items = [];
+        }
+
         $result = [];
         foreach ($items as $item) {
             if (!is_array($item)) {
@@ -25,7 +36,13 @@ class ProducerRepository extends AbstractRepository
             }
             $result[] = Producer::fromArray($item);
         }
+
         return $result;
+    }
+
+    private static function isList(array $arr): bool
+    {
+        return array_values($arr) === $arr;
     }
 
     public function createOne(Producer $producer): Producer
